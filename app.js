7e17347877d8ff53b4a0c85775cfab409b2efa00
rogerzz01 <148784099+rogerzz01@users.noreply.github.com>
2023-10-23 15:16:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_kqshqnk", "template_3hou33j", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
